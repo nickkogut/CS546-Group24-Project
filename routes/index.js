12f6df105dde3ecfa249b45cb7e147e2303d0e97
Shane Ruggirello <darkgasher2794@gmail.com>
2025-11-19 19:27:10 -0500

    Handled most of the basic front end. Setup a landing page with a persistend header bar as well as pages for what each person will work on. I haven't added the login pages yet though, not sure how we want to go about that.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,23 @@
-// import XXXRoutes from "./filename.js";
-
-
 const constructorMethod = (app) => {
-	// app.use("/routePath", XXXRoutes);
+  app.get('/', (req, res) => {
+    res.render('home', {title: 'Home | CareerScope NYC'});
+  });
 
-    app.use("{*splat}", (req, res) => {
-        res.status(404).json({ error: "Route Not found" });
-    });
+  app.get('/jobs', (req, res) => {
+    res.render('jobs', {title: 'Jobs | CareerScope NYC'});
+  });
+
+  app.get('/compare', (req, res) => {
+    res.render('compare', {title: 'Compare | CareerScope NYC'});
+  });
+
+  app.get('/account', (req, res) => {
+    res.render('account', {title: 'My Account | CareerScope NYC'});
+  });
+
+  app.use((req, res) => {
+    res.status(404).json({error: 'Route Not found'});
+  });
 };
 
 export default constructorMethod;
