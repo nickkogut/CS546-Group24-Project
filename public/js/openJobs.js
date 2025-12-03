da3a5ca59305cf28fcbec49a875cc58502988fb1
nkogut <nkogut4444@gmail.com>
2025-12-03 18:20:33 -0500

    search bug fixes

--- public/js/openJobs.js
+++ public/js/openJobs.js
@@ -34,116 +34,138 @@ $(function () {
 updateSliderDisplay(Number($("#minSalary").val()), Number($("#maxSalary").val()));
 
 (function ($) {
-  // form submission
-  $("#openJobSearch").submit(function (event) {
-    event.preventDefault();
-    $("#error").hide();
-    $("#errorList").empty();
-    $("#jobListings").empty();
-    const errors = [];
+    // form submission
+    $("#openJobSearch").submit(function (event) {
+        event.preventDefault();
+        $("#error").hide();
+        $("#errorList").empty();
+        $("#jobListings").empty();
+        const errors = [];
 
-    const minDate = $("#minDate").val();
-    if (minDate !== "") {
-        const today = new Date();
-        const formattedDate = minDate.substring(5, 7) + "/" + minDate.substring(8, 10) + "/" + minDate.substring(0, 4);
-        if (new Date(formattedDate) > today) errors.push(`<li class="error">Date cannot be in the future</li>`);
-    }
+        const minDate = $("#minDate").val();
+        if (minDate !== "") {
+            const today = new Date();
+            const formattedDate = minDate.substring(5, 7) + "/" + minDate.substring(8, 10) + "/" + minDate.substring(0, 4);
+            if (new Date(formattedDate) > today) errors.push(`<li class="error">Date cannot be in the future</li>`);
+        }
 
 
-    const title = $("#title").val().trim();
-    const agency = $("#agency").val().trim();
-    const keywords = $("#keywords").val().trim();
+        const title = $("#title").val().trim();
+        const agency = $("#agency").val().trim();
+        const keywords = $("#keywords").val().trim();
 
-    // new
-    const borough = $("#borough").val();
-    const fullTime = $("#fullTime").val();
-    const nonResidency = $("#nonResidency").val();
-    const minSalary = $("#minSalary").val();
-    const maxSalary = $("#maxSalary").val();
-    const numPerPage = $("#numPerPage").val();
-    const page = $("#page").val();
+        // new
+        const borough = $("#borough").val();
+        const fullTime = $("#fullTime").val();
+        const nonResidency = $("#nonResidency").val();
+        const minSalary = $("#minSalary").val();
+        const maxSalary = $("#maxSalary").val();
+        const numPerPage = $("#numPerPage").val();
+        let page = $("#page").val();
 
-    if (title.length > 100) errors.push(`<li class="error">Title is too long</li>`);
-    if (agency.length > 100) errors.push(`<li class="error">Agency is too long</li>`);
-    if (keywords.length > 200) errors.push(`<li class="error">Too many keywords</li>`);
+        if (page < 1) {
+            // the most recent search returned no results
+            page = 1;
+        }
 
-    // Make sure the agency and title will have matches in the database
-    const matchingAgencies = $('#agencyList option').filter(function () {
-        var re = new RegExp(agency, 'i')
-        return this.value.match(re);
-    });
+        if (title.length > 100) errors.push(`<li class="error">Title is too long</li>`);
+        if (agency.length > 100) errors.push(`<li class="error">Agency is too long</li>`);
+        if (keywords.length > 200) errors.push(`<li class="error">Too many keywords</li>`);
 
-    const matchingTitles = $('#titleList option').filter(function () {
-        var re = new RegExp(title, 'i')
-        return this.value.match(re);
-    });
-
-    if (matchingAgencies.length === 0) errors.push(`<li class="error">No matching agencies</li>`);
-    if (matchingTitles.length === 0) errors.push(`<li class="error">No matching titles</li>`);
-
-    if (errors.length > 0) {
-        $('#openJobSearch').trigger('reset'); // DEBUG
-        $("#error").show();
-        errors.forEach((e) => {
-            $("#errorList").append(e);
+        // Make sure the agency and title will have matches in the database
+        const matchingAgencies = $('#agencyList option').filter(function () {
+            var re = new RegExp(agency, 'i')
+            return this.value.match(re);
         });
 
-        // Reset slider
-        updateSliderDisplay(Number($("#minSalary").val()), Number($("#maxSalary").val()));
-        $("#salarySlider").slider("value", $("#salarySlider").slider("value"));
-        $("#salarySlider").slider("values", [Number($("#minSalary").val()), Number($("#maxSalary").val())]);
-
-        $("#pageText").text(`No results`);
-        return false;
-    }
-
-    else {
-      let requestConfig = {
-        method: 'POST',
-        url: '/jobs/search',
-        contentType: 'application/json',
-        data: JSON.stringify({
-          agency,
-          title,
-          borough,
-          minSalary,
-          maxSalary,
-          keywords,
-          nonResidency,
-          fullTime,
-          minDate,
-          numPerPage,
-          page
-        })
-      };
-
-    $.ajax(requestConfig).then(function (responseMessage) {
-        const jobDiv = $("#jobListings");
-
-        console.log(responseMessage);
-        responseMessage.jobs.map((jobListing) => {
-        let element = $(
-            `<div class="listing">
-        <p>${jobListing.title}</p>
-        </div>`
-        );
-
-        // TODO: add rest of info for job in each listing. It should match handlebars
-        jobDiv.append(element);
+        const matchingTitles = $('#titleList option').filter(function () {
+            var re = new RegExp(title, 'i')
+            return this.value.match(re);
         });
 
+        if (matchingAgencies.length === 0) errors.push(`<li class="error">No matching agencies</li>`);
+        if (matchingTitles.length === 0) errors.push(`<li class="error">No matching titles</li>`);
 
-    const pageInfo = responseMessage.pageInfo;
-    $("#page").val(pageInfo.page);
-    $("#pageText").text(`Page ${pageInfo.page}/${pageInfo.maxPage} Total: ${pageInfo.numResults} Jobs`);
-  });
-    }
-  });
+        if (errors.length > 0) {
+            $('#openJobSearch').trigger('reset'); // DEBUG
+            $("#error").show();
+            errors.forEach((e) => {
+                $("#errorList").append(e);
+            });
+
+            // Reset slider
+            updateSliderDisplay(Number($("#minSalary").val()), Number($("#maxSalary").val()));
+            $("#salarySlider").slider("value", $("#salarySlider").slider("value"));
+            $("#salarySlider").slider("values", [Number($("#minSalary").val()), Number($("#maxSalary").val())]);
+
+            $("#pageText").text(`No results`);
+            return false;
+        }
+
+        else {
+            let requestConfig = {
+                method: 'POST',
+                url: '/jobs/search',
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    agency,
+                    title,
+                    borough,
+                    minSalary,
+                    maxSalary,
+                    keywords,
+                    nonResidency,
+                    fullTime,
+                    minDate,
+                    numPerPage,
+                    page
+                })
+            };
+
+            $.ajax(requestConfig).then(function (responseMessage) {
+                const jobDiv = $("#jobListings");
+                let pageInfo;
+                console.log(responseMessage);
+
+                if (responseMessage.error) {
+                    $("#error").show();
+                    $("#errorList").append(responseMessage.error);
+                    pageInfo = {
+                        page: 0,
+                        minPage: 1,
+                        maxPage: 0,
+                        numResults: 0
+                    }
+                } else {
+                    responseMessage.jobs.map((jobListing) => {
+                        let element = $(
+                            `<div class="listing">
+                            <p>${jobListing.title}</p>
+                            </div>`
+                        );
+
+                        // TODO: add rest of info for job in each listing. It should match handlebars
+                        jobDiv.append(element);
+                    });
+                    pageInfo = responseMessage.pageInfo;
+                    pageInfo.minPage = 1;
+                }
+
+                $("#page").val(pageInfo.page);
+                $("#page").attr({
+                    "min": pageInfo.minPage,
+                    "max": pageInfo.maxPage
+                });
+                $("#pageText").text(`Page ${pageInfo.page}/${pageInfo.maxPage} Total: ${pageInfo.numResults} Jobs`);
+            });
+        }
+    });
 })(window.jQuery);
 
 $("#nextPage").click(() => {
     let page = Number($("#page").val());
     let maxPage = Number($("#page").attr('max'));
+    if (!$("#error").is(":hidden")) return; // Don't try to get the next page when the search yielded an error
     if (page < maxPage) {
         $("#page").val(page + 1);
         $("#openJobSearch").submit();
@@ -151,8 +173,10 @@ $("#nextPage").click(() => {
 });
 
 $("#prevPage").click(() => {
+    if (!$("#error").is(":hidden")) return; // Don't try to get the next page when the search yielded an error
     let page = Number($("#page").val());
-    if (page > 1) {
+    let minPage = Number($("#page").attr('min'));
+    if (page > minPage) {
         $("#page").val(page - 1);
         $("#openJobSearch").submit();
     }
--- routes/openJobs.js
+++ routes/openJobs.js
@@ -9,9 +9,11 @@ router.get("/", async (req, res) => {
     try {
         const searchResponse = await filterJobs({ page: 1, numPerPage: 10 });
         const dropdownOptions = await getDropdownOptions();
-        res.render('jobs', { title: pageTitle, jobs: searchResponse.jobs, pageInfo: searchResponse.pageInfo, dropdownOptions: dropdownOptions });
+        return res.render('jobs', { title: pageTitle, jobs: searchResponse.jobs, pageInfo: searchResponse.pageInfo, dropdownOptions: dropdownOptions });
     } catch (e) {
-        res.status(500).json({ error: e.toString() });
+        // res.status(500).json({ error: e.toString() });
+        // return res.json({ error: e.toString() }).status(404);
+        return res.render('jobs', { title: pageTitle, error: e }).status(500);
     }
 });
 
@@ -24,7 +26,7 @@ router.post("/search", async (req, res) => {
         const searchResponse = await filterJobs(searchFields);
         return res.json(searchResponse);
     } catch (e) {
-        return res.status(500).json({ error: e.toString() }); // should this throw 404 if no jobs match?
+        return res.json({ error: e.toString() }).status(404);
     }
 });
 
